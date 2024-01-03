5a70bf52e65d4c0d3c2a7f2ba291e312018b11ce
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-03 22:22:27 +0530

    Added some more relevant comments for the script of this project...

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const empties = document.querySelectorAll(".empty");
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
 
+
+// Thsi is to handle the dragover, dragenter, dragleave, and dragdrop events with relevant methods... 
 for (const empty of empties) {
   empty.addEventListener("dragover", dragOver);
   empty.addEventListener("dragenter", dragEnter);
@@ -16,10 +18,14 @@ for (const empty of empties) {
   empty.addEventListener("drop", dragDrop);
 }
 
+
+// This method is for handling the dragstart event with adding the hold class dynamically before disappearing after sometime...
 function dragStart() {
   this.className += " hold";
   setTimeout(() => (this.className = "invisible"), 0);
 }
+
+// This method is for handling the dragend event, by adding the fill class after the image is dragged completely on the particular fill container...
 function dragEnd() {
   this.className = "fill";
 }
